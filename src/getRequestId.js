ad0157685ab6a1563e45c4846de4f748c30a1f98
João Augusto <joao.augusto@foodzilla.com.br>
2021-08-28 02:03:56 -0300

    fix: get request id

--- src/getRequestId.js
+++ src/getRequestId.js
@@ -1,7 +1,14 @@
 const httpContext = require('express-http-context')
+const { v4: uuidv4 } = require('uuid')
 
 const getRequestId = () => {
-  return httpContext.get('requestId')
+  const requestId = httpContext.get('requestId') || uuidv4()
+
+  if (!httpContext.get('requestId')) {
+    httpContext.set('requestId', requestId)
+  }
+
+  return requestId
 }
 
 module.exports = getRequestId
